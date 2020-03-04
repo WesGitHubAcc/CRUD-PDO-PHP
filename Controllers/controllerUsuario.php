ff621bf07fe7ca03772a15048919c35a815f1d2b
WesleyMarquesGit <wes.academico@gmail.com>
2020-03-04 14:50:51 -0300

    add listar usuarios

--- Controllers/controllerUsuario.php
+++ Controllers/controllerUsuario.php
@@ -1,16 +1,31 @@
 <?php
+
+require_once('../conecta.php');
  
     function inserirUsuario($conexao, $array){
 
        try {
             $usuarios = $conexao->prepare("INSERT INTO usuario (nome, email, senha, telefone, dt_nascimento, endereco) VALUES (?, ?, ?, ?, ?, ?)");
             $query = $usuarios->execute($array);
-            echo 'cadastrado com sucesso!';
             return $query;
-            
         }catch(PDOException $event) {
             echo 'Error: ' . $event->getMessage();
         }
+    }
+
+    function listarUsuarios($conexao){
+
+        try {
+          $usuarios = $conexao->prepare("SELECT * FROM usuario");      
+          $usuarios->execute();
+          $query = $usuarios->fetchAll(); 
+          return $query;
+
+          
+        }catch(PDOException $event) {
+              echo 'Error: ' . $event->getMessage();
+        }  
+  
     }
 
 ?>
--- View/viewUsuario.php
+++ View/viewUsuario.php
@@ -1,4 +1,9 @@
 <html>
+    
+    <?php
+      include_once('../conecta.php');
+      include_once('../Controllers/controllerUsuario.php');
+    ?>
     <link rel="stylesheet" href="">
     <title>Cadastrar Usuário</title>
 </head>
@@ -20,5 +25,50 @@
     </fieldset>
     </section>
 
+    <section class="userArea">
+    <fieldset>
+    <legend>Lista de Usuários</legend>
+
+    <?php
+
+        $usuarios = listarUsuarios($conexao);
+
+        if(empty($usuarios)){
+            ?>
+                <section>
+                    <p>Não há usuários cadastrados.</p>
+                </section>
+            <?php
+        }
+        foreach($usuarios as $usuario){
+                 
+            ?>
+                <section class="userCard">
+                    <p>Nome: <?php echo $usuario['nome']; ?></p>
+                    <p>Email <?php echo $usuario['email']; ?></p>
+                    <p>Endereço: <?php echo $usuario['endereco']; ?></p>
+                    <p>Telefone: <?php echo $usuario['telefone']; ?></p>
+                    <p>Data Nascimento: <?php echo $usuario['dt_nascimento']; ?></p>
+                    <form action="" method="post">
+                        <button type="submit" name="editar" value="<?php echo $usuario['id']; ?>"> Editar </button>
+                        <button type="submit" name="deletar" value="<?php echo $usuario['id']; ?>" onclick = "return confirma_excluir()"> Deletar </button> 
+                    </form>
+                    <br>                                                 
+                </section>
+            <?php
+        }
+    ?>
+
+    </fieldset>
+    </section>
+
 </body>
 </html>
+
+<style>
+  .userCard{
+    background-color: #f1eeee;
+    border-radius: 3px;
+  }
+
+</style>
